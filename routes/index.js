b511ae794e0139cf5f080a9494df61e165a93b9d
Code-alaly <dubiszd@gmail.com>
2020-07-19 21:47:48 -0700

    did some more work on making generic pages

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+    var context = {}
+    context.title = 'Video Games'
+    context.description = 'This page will be for showing the video games that we have in the database'
+
+
+    res.render('index', context);
+
+
 });
-router.get('/reviews', function(req, res, next) {
-  res.render('index', { title: 'Reviews' });
+router.get('/review', function (req, res, next) {
+    var context = {}
+    context.title = 'Reviews'
+    context.description = 'This page will be for showing the reviews that we have, plus adding some on'
+
+
+    res.render('review', context);
+});
+
+router.get('/dev', function (req, res, next) {
+    var context = {}
+    context.title = 'Developers'
+    context.description = 'This page will be for showing the Developers that we have, I guess we need to be able to' +
+        'remove them as well?'
+
+
+    res.render('review', context);
 });
 
 module.exports = router;
