49526009c379d90731b447488cca7188fa6379b3
Code-alaly <dubiszd@gmail.com>
2020-07-20 18:09:01 -0700

    make some changes Co-authored-by: parsonsz <parsonsz@oregonstate.edu>

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Reviews
 Video games
 Developers
 Genres
+
+Things to Add:
+add reviews
+delete reviews
+update reviews
+add developers (non user)
+update devs? (if name changes)
+add games (non user)
+delete games (to satisfy M:M delete)
+add genres (non user)
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,27 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
 
     var context = {}
-    context.title = 'Video Games'
-    context.description = 'This page will be for showing the video games that we have in the database'
+    context.title = 'About'
+    context.description = 'For our adventurous pc gamers, we encourage you to explore this database and find what games are similar to your favorites in order to further expand your gaming library! Feel free to click on each tab to search'
 
 
     res.render('index', context);
 
 
 });
+// Gets the games page
+router.get('/games', function (req, res, next) {
+
+    var context = {}
+    context.title = 'Video Games'
+    context.description = 'This page will be for showing the video games that we have in the database'
+
+
+    res.render('games', context);
+
+
+});
+// gets the review page
 router.get('/review', function (req, res, next) {
     var context = {}
     context.title = 'Reviews'
@@ -21,12 +34,22 @@ router.get('/review', function (req, res, next) {
 
     res.render('review', context);
 });
-
+// gets the dev page
 router.get('/dev', function (req, res, next) {
     var context = {}
     context.title = 'Developers'
     context.description = 'This page will be for showing the Developers that we have, I guess we need to be able to' +
         'remove them as well?'
+
+
+    res.render('review', context);
+});
+// get genres
+
+router.get('/genres', function (req, res, next) {
+    var context = {}
+    context.title = 'Genres'
+    context.description = 'This page will be for showing the Genres that we have.'
 
 
     res.render('review', context);
