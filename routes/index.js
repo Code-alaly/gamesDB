9e7b9e7049100bcd4fad66d277990c1cf1b7facf
Zach Parsons <zachparsons55@yahoo.com>
2020-08-03 21:39:02 -0700

    added database implementation for dropdown menus and other pages besides video games to display database content

--- routes/index.js
+++ routes/index.js
@@ -39,50 +39,58 @@ router.get('/games', function (req, res, next) {
         context.description = 'This page will be for showing the video games that we have in the database'
         res.render('games', context);
     });
-
-
-
-
-
 });
+
 // gets the review page
+
 router.get('/reviews', function (req, res, next) {
     var context = {}
-    context.title = 'Reviews'
-    context.description = 'This page will be for showing the reviews that we have, plus adding some on'
-
-
-    res.render('reviews', context);
+    mysql.pool.query('SELECT (SELECT name FROM Video_games WHERE Video_games.gameID = Reviews.gameID) as game, content, rating FROM Reviews', function(err, rows, fields) {
+        results = rows
+        context.results = results
+        context.title = 'Reviews'
+        context.description = 'This page will be for showing the reviews that we have, plus adding some on'
+        res.render('reviews', context);
+    });
 });
+
 // gets the dev page
+
 router.get('/devs', function (req, res, next) {
     var context = {}
-    context.title = 'Developers'
-    context.description = 'This page shows the Developers. \n You can search for developers and update names if they change.'
-
-
-    res.render('devs', context);
+    mysql.pool.query('SELECT (SELECT name FROM Video_games WHERE Video_games.gameID = Developers.gameID) as game, name, size FROM Developers', function(err, rows, fields) {
+        results = rows
+        context.results = results
+        context.title = 'Developers'
+        context.description = 'This page shows the Developers. \n You can search for developers and update names if they change.'
+        res.render('devs', context);
+    });
 });
+
 // get genres
 
 router.get('/genres', function (req, res, next) {
     var context = {}
-    context.title = 'Genres'
-    context.description = 'This page shows different genres. This includes the Genre name and a description.'
-
-
-    res.render('genres', context);
+    mysql.pool.query('SELECT * FROM Genres', function(err, rows, fields){
+        results = rows
+        context.results = results
+        context.title = 'Genres'
+        context.description = 'This page shows different genres. This includes the Genre name and a description.'
+        res.render('genres', context);
+    });
 });
 
 // get games_genres
 
 router.get('/games_genres', function (req, res, next) {
     var context = {}
-    context.title = 'Games-Genres'
-    context.description = 'This page shows video games and their corresponding genre(s).'
-
-
-    res.render('games_genres', context);
+    mysql.pool.query('SELECT (SELECT name FROM Video_games WHERE Video_games.gameID = games_genres.gameID) as game, (SELECT name FROM Genres WHERE Genres.genreID = games_genres.genreID) as genre FROM games_genres', function(err, rows, fields) {
+        results = rows
+        context.results = results
+        context.title = 'Games-Genres'
+        context.description = 'This page shows video games and their corresponding genre(s).'
+        res.render('games_genres', context);
+    });
 });
 
 // router.get('/test', function (req, res, next) {
