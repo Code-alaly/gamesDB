15ecfce0517ca6c012a4eb6a5ddd471f5e8718f0
Code-alaly <dubiszd@gmail.com>
2020-08-10 17:58:02 -0700

    starting on getting review edit to work

--- public/javascripts/review.js
+++ public/javascripts/review.js
@@ -15,7 +15,7 @@ $(function () {
             "                {{rating}}\n" +
             "            </td>\n" +
             "            <td>\n" +
-            "                <button type=\"button\" class=\"btn btn-teal btn-rounded btn-sm m-0 \">Edit Name</button>\n" +
+            "                <button type=\"button\" class=\"btn btn-teal btn-rounded btn-sm m-0 edit\">Edit Name</button>\n" +
             "            </td>\n" +
             "            <td>\n" +
             "                <button type=\"button\" class=\"btn btn-info btn-rounded btn-sm m-0 remove\" data-id='{{reviewID}}'>Remove</button>\n" +
@@ -23,6 +23,33 @@ $(function () {
             "        </tr>"
     var g_template =
         "<option>{{name}}</option>"
+
+    var editTemplate = " <tr>\n" +
+        "            <td>\n" +
+        "                <span>\n" +
+        "                Review:\n" +
+        "            </span>\n" +
+        "                <input class=\" form-control name\">\n" +
+        "            </td>\n" +
+        "            <td>\n" +
+        "                <span>\n" +
+        "                    Rating:\n" +
+        "                </span>\n" +
+        "                <select class=\"form-control rate\">\n" +
+        "                    <option>1</option>\n" +
+        "                    <option>2</option>\n" +
+        "                    <option>3</option>\n" +
+        "                    <option>4</option>\n" +
+        "                    <option>5</option>\n" +
+        "                </select>\n" +
+        "            </td>\n" +
+        "            <td>\n" +
+        "                <button type=\"button\" class=\"btn btn-teal btn-rounded btn-sm m-0\">Submit</button>\n" +
+        "            </td>\n" +
+        "            <td>\n" +
+        "                <button type=\"button\" class=\"btn btn-info btn-rounded btn-sm m-0\" >Cancel</button>\n" +
+        "            </td>\n" +
+        "        </tr>"
         // $(".reviews").html()
 
         function addGame(game) {
@@ -82,5 +109,17 @@ $(function () {
 
         })
             .done($tr.remove())
+    })
+
+    $table.on('click', '.edit', function () {
+        var $tr = $(this).closest('tr')
+        $tr.append(editTemplate)
+        // $.ajax({
+        //     type: 'delete',
+        //     url: '/reviews-del',
+        //     data: {id: $(this).attr('data-id')}
+        //
+        // })
+            // .done($tr.remove())
     })
 })
